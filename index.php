75fb8884ff227d2cbef934a900e36a5749f80501
blindmelon07 <bryandetera07@gmail.com>
2024-04-22 22:31:44 +0800

    johnpaulday:1

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+//declaration of variables
+    $bryan = "buang";
+    $bryan1= "gwapo";
+
+ //printing of variables
+    echo $bryan;
+    print $bryan1;
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<input type="text" name="bryan"  value="<?php echo $bryan;?>"  id="">
+</body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,90 @@
+/*  const myObj = { name: 'John', age: 30 };
+console.log(myObj.age); // John
+
+  */
+//declare a variable with an object
+//parent = const myGender
+//child =   Sex: 'Male',
+    //Age: 30,
+    //Location: 'USA',
+   // Occupation: 'Engineer'
+ 
+ /*   let myObj ={
+        johnpaul : {
+        id: 1,
+        transactionNumber: 123123123,
+        date: '2024-10-10',
+        Location: ['salog', 'balogo', 'UK'],
+        branch: ['sorsogon', 'manila', 'donsol'],
+         },
+      bryan : {
+        id: 2,
+        transactionNumber: 2896546541352,
+        date: '2021-10-10',
+        Location: ['USA', 'Canada', 'UK'],
+        branch: ['sorsogon', 'manila', 'pilar'],
+    }
+   };
+ 
+    
+console.log(myObj.bryan);  */// 30 */
+
+//oprators in javascript
+
+//The Assignment Operator = assigns values
+
+//The Addition Operator + adds values
+/* let x = 5;
+let y = 2;
+
+console.log(x + y ); */
+//The Multiplication Operator * multiplies values
+/* let jp = 5;
+let jona = 10;
+console.log(jona * jp); */
+//The Comparison Operator > compares values
+/*  let x = 2;
+    let y = 3;
+    let z = 2;
+    console.log( x < y || x == z); */
+    //Arithmetic Operators Example
+/*      let x = 3;
+    let y = (100 / 50) * x;
+    console.log(y); // 450
+ */
+//data types
+// Numbers:
+/* let length = 16;
+let weight = 7.5;
+//Strings:
+let name = 'John';
+let carName = 'Volvo';
+//Booleans:
+let x = 5;
+let y = 5;
+let z = 6;
+console.log(x == y); // Returns true
+console.log(x == z); // Returns false
+//Arrays:
+let cars = [
+    //index: value
+    'Saab', //[0]
+    'Volvo', //[1]
+    'BMW'//[2]
+];
+//Objects:
+let person = {
+    //key: value
+    firstName: 'John', 
+    lastName: 'Doe', 
+    age: 50
+};
+//Undefined
+let car;
+console.log(car); // Returns undefined
+//Null
+let person = {firstName: 'John', lastName: 'Doe', age: 50, eyeColor: 'blue'};
+person = null; // Now value is null, but type is still an object */
+//typeof Operator
+let person = 10;
+console.log(typeof person); // Returns string
